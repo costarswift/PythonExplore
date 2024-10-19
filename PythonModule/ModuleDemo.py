a3288838e3992a03848e6c0cbda1a1faed0c9304
Costar <costarswift@gmail.com>
2024-10-19 17:32:52 +0800

    #Python 进阶 模块

---
+++ PythonModule/ModuleDemo.py
@@ -0,0 +1,8 @@
+import my_module2
+import my_module1
+
+ans = MyModule.add(4, 8)
+print(ans)
+
+num = my_module1.test_a(5, 6)
+print(num)
---
+++ PythonModule/my_module1.py
@@ -0,0 +1,10 @@
+def test_a(a, b):
+    print(a + b)
+
+
+def test_b(a, b):
+    print(a - b)
+
+if __name__ == '__main__':
+    num = test_a(1, 2)
+    print(num)
